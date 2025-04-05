745332ce0e6b5a12496f135e2f608ed533af5eaf
Dibyendu Mukherjee <dibyendu7in@gmail.com>
2025-04-05 17:14:34 +0530

    commit

--- create_tables.sql
+++ create_tables.sql
@@ -51,3 +51,53 @@ CREATE TABLE payments (
     payment_method VARCHAR(50) CHECK (payment_method IN ('credit card', 'debit card', 'UPI', 'wallet')),
     transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE drivers (
+    driver_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    phone VARCHAR(15) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    password_hash TEXT NOT NULL,
+    license_number VARCHAR(50),
+    vehicle_id INTEGER REFERENCES vehicles(vehicle_id),
+    is_available BOOLEAN DEFAULT TRUE,
+    current_lat DOUBLE PRECISION,
+    current_lng DOUBLE PRECISION,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE driver_earnings (
+    earning_id SERIAL PRIMARY KEY,
+    driver_id INTEGER REFERENCES drivers(driver_id),
+    booking_id INTEGER REFERENCES bookings(booking_id),
+    amount NUMERIC(10, 2),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE driver_sessions (
+    session_id SERIAL PRIMARY KEY,
+    driver_id INT REFERENCES drivers(driver_id),
+    login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    logout_time TIMESTAMP
+);
+
+CREATE TABLE repayment_history (
+    repayment_id SERIAL PRIMARY KEY,
+    purchase_id INT REFERENCES vehicle_purchase(purchase_id),
+    booking_id INT REFERENCES bookings(booking_id),
+    amount NUMERIC(10,2),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE ride_feedback (
+    feedback_id SERIAL PRIMARY KEY,
+    booking_id INT UNIQUE REFERENCES bookings(booking_id),
+    user_id INT REFERENCES users(user_id),
+    driver_id INT REFERENCES drivers(driver_id),
+    rating INT CHECK (rating BETWEEN 1 AND 5) NOT NULL,
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
